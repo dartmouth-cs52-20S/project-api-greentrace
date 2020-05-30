20630d8faad9be1a4c5e0e13de2e36537c96a61b
Aditya Choudhari <adityachoudhari26@gmail.com>
2020-05-29 21:03:05 -0400

    added algorithm stuff

--- src/controllers/observation-controller.js
+++ src/controllers/observation-controller.js
@@ -28,7 +28,28 @@ const identifyContact = (req, res) => {
 //            record the current time as the initial point of contact
 //            record the end time initially as null
 //        else create a two-way contact
-}
+//            const contactA (current user as secondary source) = new Contact ()
+//            const contactB (current user as primary source) = new Contact ()
+//            follow the same process above... subbing in contactA or contactB and making the appropriate changes
+//            contactB adjustments: switch primary/secondary source, fetch the other persons timestamp for that location,
+//                                  record the end time as the time when
+//                                  need to record whether contact is two way or not
+//                                  when we update contactA, if it is a two way contact then
+//                                      query by switching primary and secondary (.find())
+//                                      if there's multiple, just get the one that has no end time, and update it's endtime
+};
+
+const updateContact = (req, res) => {
+  // if there is a previous contact
+  // purpose: complete the previous contact with the current time as exit
+};
+
+const setExitTimeStamp = () => {
+  // if first entry
+  //     ignore
+  // else
+  //     update the last entry's endtime with the current time of the new observation
+};
 
 export const getDataTimestamp = (req, res) => {
   return req.body.dataCollectionTimestamp;
