2ad90d59c6f859281c98df6bc622df24c989c0f2
David Kantor <david.a.kantor.22@dartmouth.edu>
2020-05-31 15:58:49 -0400

    adding user-model too

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -5,10 +5,10 @@ import mongoose, { Schema } from 'mongoose';
 // import bcrypt from 'bcryptjs';
 
 const MessageSchema = new Schema({
-  traceID: { type: String },
+  // traceID: { type: String },
   tested: { type: Boolean },
   covid: { type: Boolean },
-  // timestamp: { type: String },
+  timestamp: { type: String },
   contactDate: { type: String },
 });
 
